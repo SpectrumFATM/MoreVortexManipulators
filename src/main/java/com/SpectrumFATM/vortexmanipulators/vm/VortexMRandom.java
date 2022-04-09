a0c7209ec22b12971c3873cb07327607d790a379
Spectrum <53658189+SpectrumFATM@users.noreply.github.com>
2022-04-09 09:26:15 +0100

    Fixed randomiser function.

--- src/main/java/com/SpectrumFATM/vortexmanipulators/vm/VortexMRandom.java
+++ src/main/java/com/SpectrumFATM/vortexmanipulators/vm/VortexMRandom.java
@@ -1,20 +1,11 @@
 package com.SpectrumFATM.vortexmanipulators.vm;
 
-import com.SpectrumFATM.vortexmanipulators.VortexM;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.tardis.mod.helper.PlayerHelper;
-import net.tardis.mod.helper.TextHelper;
-import net.tardis.mod.misc.SpaceTimeCoord;
 import net.tardis.mod.network.TPacketHandler;
 import net.tardis.mod.vm.BaseVortexMFunction;
-import org.lwjgl.system.CallbackI;
 
 import java.util.Random;
 
@@ -28,8 +19,17 @@ public class VortexMRandom extends BaseVortexMFunction {
     @Override
     public void sendActionOnServer(World world, ServerPlayerEntity player) {
         super.sendActionOnClient(world, player);
-        int x = random.nextInt(100);
-        int y = random.nextInt(100);
+        int positive = random.nextInt(1);
+        double x;
+        double y;
+
+        if (positive == 1) {
+           x = player.getX() + random.nextInt(100);
+           y = player.getY() + random.nextInt(100);
+        } else {
+            x = player.getX() - random.nextInt(100);
+            y = player.getY() - random.nextInt(100);
+        }
 
         TPacketHandler.handleVortexMTeleport(player, new BlockPos(x, 64, y), false);
         PlayerHelper.closeVMModel(player);
